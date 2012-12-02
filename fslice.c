341cbedd3b182e55e1a7bf5cc078672f4bc6ab8c
Ander <zman7777@hotmail.es>
2012-12-02 19:01:10 +0100

    Added create command, structure for delete command

--- fslice.c
+++ fslice.c
@@ -2,18 +2,72 @@
 
 int main(int argc,char **argv)
 {
+	char *fname;
+
+	FILE *f1;
+
 	//Display name & version
 	printf("%s %s\n",FSLICE_NAME,FSLICE_VERSION);
 
 	//If no arguments passed
 	if(argc<2)
 	{
-		printf("Error, command missing. Write \"%s %s\" for help.\n",argv[0],FSLICE_COM_HELP);
+		printf(FSLICE_MSG_NO_COMMAND,argv[0],FSLICE_COM_HELP);
 		return 1;
 	}
 
 	//Display program help
 	if(strcmp(argv[1],FSLICE_COM_HELP)==0)	printf(FSLICE_MSG_HELP);
+
+	//Create new file
+	if(strcmp(argv[1],FSLICE_COM_CREATE)==0)
+	{
+		printf(FSLICE_MSG_CREATE);
+
+		//If no filename passed
+		if(argc<3)
+		{
+			printf(FSLICE_MSG_NO_FILENAME);
+			return 2;
+		}
+
+		//Check if file exists
+		f1=fopen(argv[2],"rb+");
+		if(f1!=NULL)
+		{
+			fclose(f1);
+			printf(FSLICE_MSG_CREATE_EXISTS);
+			return 3;
+		}
+
+		//Create file
+		f1=fopen(argv[2],"wb+");
+
+		//Check if file created successfully
+		if(f1==NULL)
+		{
+			printf(FSLICE_MSG_CREATE_ERR);
+			return 4;
+		}
+
+		//Close file
+		fclose(f1);
+	}
+
+	//Delete existing file
+	if(strcmp(argv[1],FSLICE_COM_DELETE)==0)
+	{
+		printf(FSLICE_MSG_DELETE);
+
+		//If no filename passed
+		if(argc<3)
+		{
+			printf(FSLICE_MSG_NO_FILENAME);
+			return 2;
+		}
+
+		
+	}
 
 	return 0;
 }
--- fslice.h
+++ fslice.h
@@ -4,14 +4,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 #define FSLICE_NAME "FSlice by Mwitishi"
 #define FSLICE_VERSION "v0.0"
 
 #define FSLICE_COM_HELP "help"
+#define FSLICE_COM_CREATE "create"
+#define FSLICE_COM_DELETE "delete"
 
+#define FSLICE_MSG_NO_COMMAND "Error, command missing. Write \"%s %s\" for help.\n"
 #define FSLICE_MSG_HELP "This program allows you to read and modify files.\n\
 These are the available commands:\n\
-help: display this help.\n"
+help: display this help.\n\
+create <filename>: create a file with specified name.\n"
+#define FSLICE_MSG_CREATE "Attempting to create file...\n"
+#define FSLICE_MSG_NO_FILENAME "Error, no filename specified.\n"
+#define FSLICE_MSG_CREATE_EXISTS "Error, the specified file already exists.\n"
+#define FSLICE_MSG_CREATE_ERR "Error while creating file.\n"
+#define FSLICE_MSG_DELETE "Attempting to delete file...\n"
+#define FSLICE_MSG_DELETE_ERR "Error while deleting file.\n"
 
 #endif
