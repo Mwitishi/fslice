38a80abfab565359d8c9ffa3c977274492c193de
Ander <zman7777@hotmail.es>
2012-12-02 18:28:15 +0100

    Added help command

--- fslice.c
+++ fslice.c
@@ -2,11 +2,18 @@
 
 int main(int argc,char **argv)
 {
+	//Display name & version
+	printf("%s %s\n",FSLICE_NAME,FSLICE_VERSION);
+
+	//If no arguments passed
 	if(argc<2)
 	{
 		printf("Error, command missing. Write \"%s %s\" for help.\n",argv[0],FSLICE_COM_HELP);
 		return 1;
 	}
+
+	//Display program help
+	if(strcmp(argv[1],FSLICE_COM_HELP)==0)	printf(FSLICE_MSG_HELP);
 
 	return 0;
 }
--- fslice.h
+++ fslice.h
@@ -3,9 +3,15 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+#define FSLICE_NAME "FSlice by Mwitishi"
 #define FSLICE_VERSION "v0.0"
 
 #define FSLICE_COM_HELP "help"
+
+#define FSLICE_MSG_HELP "This program allows you to read and modify files.\n
+			 These are the available commands:
+			 help: display this help"
 
 #endif
