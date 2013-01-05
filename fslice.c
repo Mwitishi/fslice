a0ad8791f3d7af024d83966849bbd7e7753a750c
Mwitishi <mwitishi@gmail.com>
2013-01-05 18:34:01 +0100

    Added structure for slice command, fslice_write function, fslice_tbytes global variable

--- fslice.c
+++ fslice.c
@@ -1,7 +1,57 @@
 #include "fslice.h"
 
+//Global variable for counting bytes printed so far
+int fslice_tbytes;
+
+/*
+ *  Function for printing the contents of the buffer in specified visualization mode.
+ *  size is the number of bytes to be written (normally FSLICE_BUF_SIZE).
+ */
+int fslice_write(char *vmode,char *buf,int size)
+{
+    int i1,i2;
+
+    //Check for NULL pointers
+    if(vmode==NULL||buf==NULL)
+    {
+        printf(FSLICE_MSG_NULL_POINTER);
+        return 1;
+    }
+
+    //Loop through the buffer
+    for(i1=0;i1<size;i1++)
+    {
+        //Print bytes as characters
+        if(strcmp(vmode,FSLICE_VMODE_CHAR)==0)
+            printf("%c",buf[i1]);
+
+        //Print byte values as numbers
+        if(strcmp(vmode,FSLICE_VMODE_NUM)==0)
+            printf("%d",(int)buf[i1]);
+
+        //Print full format
+        if(strcmp(vmode,FSLICE_VMODE_FULL)==0)
+        {
+            //Print the data, calculate characters to fill
+            i2=FSLICE_DISPL_DIST-(printf(FSLICE_FULL_FORMAT,fslice_tbytes,(int)buf[i1]))%FSLICE_DISPL_DIST;
+
+            //Newline after writing FSLICE_DISPL_COLS entries in each line
+            if((fslice_tbytes+1)%FSLICE_DISPL_COLS==0) printf("\n");
+
+            //Fill space until entry reaches FSLICE_DISPL_DIST length
+            else printf("%*s",i2,"");
+        }
+
+        //Increase count of bytes written
+        fslice_tbytes++;
+    }
+
+    return 0;
+}
+
 int main(int argc,char **argv)
 {
+    char *buf=(char*)malloc(FSLICE_BUF_SIZE);
     FILE *f1;
 
     //Display name & version
@@ -74,7 +124,7 @@ int main(int argc,char **argv)
         f1=fopen(argv[2],"rb+");
         if(f1==NULL)
         {
-            printf(FSLICE_MSG_DELETE_NEXISTS);
+            printf(FSLICE_MSG_NEXISTS);
             return 3;
         }
 
@@ -86,6 +136,17 @@ int main(int argc,char **argv)
         {
             printf(FSLICE_MSG_DELETE_ERR);
             return 4;
+        }
+
+        return 0;
+    }
+
+    if(strcmp(argv[1],FSLICE_COM_SLICE)==0)
+    {
+        if(argc<3)
+        {
+            printf(FSLICE_MSG_NO_FILENAME);
+            return 2;
         }
 
         return 0;
--- fslice.h
+++ fslice.h
@@ -10,9 +10,21 @@
 #define FSLICE_NAME "FSlice by Mwitishi"
 #define FSLICE_VERSION "v0.1"
 
+#define FSLICE_BUF_SIZE 20
+#define FSLICE_DISPL_COLS 4
+#define FSLICE_DISPL_DIST 20
+
 #define FSLICE_COM_HELP "help"
 #define FSLICE_COM_CREATE "create"
 #define FSLICE_COM_DELETE "delete"
+#define FSLICE_COM_SLICE "slice"
+#define FSLICE_COM_WRITE "write"
+
+#define FSLICE_VMODE_CHAR "char"
+#define FSLICE_VMODE_NUM "num"
+#define FSLICE_VMODE_FULL "full"
+
+#define FSLICE_FULL_FORMAT "Position %d:%d"
 
 #define FSLICE_MSG_NO_COMMAND "Error, command missing. Write \"%s %s\" for help.\n"
 #define FSLICE_MSG_INVCOMMAND "Error, unable to recognise the issued command.\n"
@@ -23,10 +35,12 @@ create <filename>: create a file with specified name.\n\
 delete <filename>: delete the file with specified name.\n"
 #define FSLICE_MSG_CREATE "Attempting to create file...\n"
 #define FSLICE_MSG_NO_FILENAME "Error, no filename specified.\n"
+#define FSLICE_MSG_NEXISTS "Error, the specified file doesn\'t exist.\n"
+#define FSLICE_MSG_INVPOS "Error, the specified position exceeds the file size.\n"
+#define FSLICE_MSG_NULL_POINTER "Error, received a NULL pointer.\n"
 #define FSLICE_MSG_CREATE_EXISTS "Error, the specified file already exists.\n"
 #define FSLICE_MSG_CREATE_ERR "Error while creating file.\n"
 #define FSLICE_MSG_DELETE "Attempting to delete file...\n"
-#define FSLICE_MSG_DELETE_NEXISTS "Error, the specified file doesn\'t exist.\n"
 #define FSLICE_MSG_DELETE_ERR "Error while deleting file.\n"
 
 #endif
